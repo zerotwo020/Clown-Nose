db89c28caa934bd511d26afdcedaacc7ba38a998
zerotwo020 <85903183+zerotwo020@users.noreply.github.com>
2022-03-02 17:34:30 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
+//https://i.postimg.cc/HnBsFQqn/Red-Nose.png
+
+noseX=0;
+noseY=0;
+
 function preload() {
+    clown_nose=loadImage('https:i.postimg.cc/HnBsFQqn/Red-Nose.png')
 }
  function setup() {
      canvas=createCanvas(300,300);
@@ -13,6 +19,8 @@ function preload() {
 
  function draw() {
      image(video,0,0,300,300);
+     image(clown_nose, noseX -12 ,noseY -8,30,30);
+     
  }
  function take_snapshot() {
 save('myFilterImage.png');
@@ -28,7 +36,9 @@ save('myFilterImage.png');
  function gotPoses(results) {
 if(results.length > 0){
     console.log(results);
-    console.log("nose x = " + results[0].pose.nose.x);
-    console.log("nose y = " + results[0].pose.nose.y);
+    noseX= results[0].pose.nose.x
+    noseY= results[0].pose.nose.y
+    console.log("nose x = " + noseX);
+    console.log("nose y = " +  noseY);
 }
  }
